395a29ff8985acc481f5a3d02fb8b4b058aa4cac
Sachitha Hirushan <shpsachitha@gmail.com>
2021-02-04 12:12:00 +0530

    CSS wip

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     //TODO what is BEM convention
     <div className="app">
-      <h1>Hello </h1>
       {/* Header */}
+      <Header/>
       {/* Home */}
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,32 @@ function Header() {
         <div className="header">
             <img src="http://pngimg.com/uploads/amazon/amazon_PNG6.png" className="header__logo" />
             <div className="header__search">
-                
+                <input 
+                    className="header__searchInput" type="text"
+                    
+                    />
+                {/* LOGO */}
+            </div>
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Sachitha</span>
+                    <span className="header__optionLineTwo">Sign In</span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Sachitha</span>
+                    <span className="header__optionLineTwo">Sign In</span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Sachitha</span>
+                    <span className="header__optionLineTwo">Sign In</span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Sachitha</span>
+                    <span className="header__optionLineTwo">Sign In</span>
+                </div>
             </div>
 
         </div>
