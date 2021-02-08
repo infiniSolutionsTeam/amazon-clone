b3b4fbba4efd342d019652107cd7e25749e8dca9
Sachitha Hirushan <shpsachitha@gmail.com>
2021-02-08 19:21:43 +0530

    shoping cart done

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ function App() {
               <Route path="/checkout">
                 <Checkout/>
               </Route>
+              <Route path="/login">
+                <h1>Login page</h1>
+              </Route>
               <Route path="/">
                 <Home/>
               </Route>
--- src/Header.js
+++ src/Header.js
@@ -21,10 +21,12 @@ function Header() {
                 {/* LOGO */}
             </div>
             <div className="header__nav">
-                <div className="header__option">
-                    <span className="header__optionLineOne">Hello Guset</span>
-                    <span className="header__optionLineTwo">Sign In</span>
-                </div>
+                <Link to="login">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Hello Guset</span>
+                        <span className="header__optionLineTwo">Sign In</span>
+                    </div>
+                </Link>
 
                 <div className="header__option">
                     <span className="header__optionLineOne">Returns</span>
